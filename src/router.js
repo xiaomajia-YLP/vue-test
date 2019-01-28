ec1cb3f64ef8019a5c4f988f86a27c7f6cfe5490
xiaomajia <1134126206@qq.com>
2019-01-28 09:55:19 +0800

    update

--- src/router.js
+++ src/router.js
@@ -1,18 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 console.log(process.env);
 
-export default new Router({
+let router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
-      component: Home
+      component: ()=>import(/* webpackChunkName: "home" */ './views/Home.vue'),
+      meta: {
+        title: '首页'
+      }
     },
     {
       path: '/about',
@@ -21,7 +22,25 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       // webpack 提供 Magic Comments 给拆分出的文件命名 --> webpackChunkName
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue'),
+      meta: {
+        title: '关于'
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  let title = to.meta && to.meta.title
+  if (title) {
+    document.title = title
+  }
+  if (to.name === 'home') {
+    // 拦截并跳转至 page2 单页
+    Vue.$openRouter({
+      name: 'page2'
+    });
+  }
+})
+
+export default router
